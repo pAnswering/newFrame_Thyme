971920bff80a18019c9e3692db7a0f9d05d48067
Seongjin <hiysr0308@koreatech.ac.kr>
2017-09-25 02:04:29 +0900

    Create NewFrame TAS file
    Newframe_tas.js -> collect sensing data from DHT11
    conf.xml -> configuration file for TAS

---
+++ Newframe_tas.js
@@ -0,0 +1,156 @@
+/**
+ * Created by ryeubi on 2015-08-31.
+ */
+
+var net = require('net');
+var util = require('util');
+var fs = require('fs');
+var xml2js = require('xml2js');
+var exec = require("child_process").exec;
+var sensorLib = require('node-dht-sensor');
+//var sh_serial = require('./serial');
+
+var useparentport = '';
+var useparenthostname = '';
+
+var upload_arr = [];
+var download_arr = [];
+
+// This is an async file read
+fs.readFile('conf.xml', 'utf-8', function (err, data) {
+    if (err) {
+        console.log("FATAL An error occurred trying to read in the file: " + err);
+        console.log("error : set to default for configuration")
+    }
+    else {
+        var parser = new xml2js.Parser({explicitArray: false});
+        parser.parseString(data, function (err, result) {
+            if (err) {
+                console.log("Parsing An error occurred trying to read in the file: " + err);
+                console.log("error : set to default for configuration")
+            }
+            else {
+                var jsonString = JSON.stringify(result);
+                conf = JSON.parse(jsonString)['m2m:conf'];
+
+                useparenthostname = conf.tas.parenthostname;
+                useparentport = conf.tas.parentport;
+
+                if(conf.upload != null) {
+                    if (conf.upload['ctname'] != null) {
+                        upload_arr[0] = conf.upload;
+                    }
+                    else {
+                        upload_arr = conf.upload;
+                    }
+                }
+
+                if(conf.download != null) {
+                    if (conf.download['ctname'] != null) {
+                        download_arr[0] = conf.download;
+                    }
+                    else {
+                        download_arr = conf.download;
+                    }
+                }
+
+                //sh_serial.open(usecomport, usebaudrate);
+            }
+        });
+    }
+});
+
+
+var tas_state = 'connect';
+
+var upload_client = new net.Socket();
+
+upload_client.on('data', function(data) {
+
+    if (tas_state == 'connect' || tas_state == 'reconnect' || tas_state == 'upload') {
+        var data_arr = data.toString().split('}');
+        for(var i = 0; i < data_arr.length-1; i++) {
+            var line = data_arr[i];
+            line += '}';
+            var sink_str = util.format('%s', line.toString());
+            var sink_obj = JSON.parse(sink_str);
+
+            if (sink_obj.ctname == null || sink_obj.con == null) {
+                console.log('Received: data format mismatch');
+            }
+            else {
+                if (sink_obj.con == 'hello') {
+                    console.log('Received: ' + data);
+
+                    if (++tas_man_count >= download_arr.length) {
+                        tas_state = 'upload';
+                    }
+                }
+                else {
+                    for (var j = 0; j < upload_arr.length; j++) {
+                        if (upload_arr[j].ctname == sink_obj.ctname) {
+                            console.log('ACK : ' + line + ' <----');
+                            break;
+                        }
+                    }
+
+                    for (j = 0; j < download_arr.length; j++) {
+                        if (download_arr[j].ctname == sink_obj.ctname) {
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
+
+
+var sensor = {
+ initialize: function () {
+   return sensorLib.initialize(11, 7); // dht version: 11, using 25 pin
+ },
+ read: function () {
+   var readout = sensorLib.read();
+
+   if(tas_state == 'upload') {
+       for(var i = 0; i < upload_arr.length; i++) {
+           if(upload_arr[i].ctname == 'cnt-temp') {
+               var cin = {ctname: upload_arr[i].ctname, con: readout.temperature.toFixed(2)};
+               console.log('SEND : ' + JSON.stringify(cin) + ' ---->');
+               upload_client.write(JSON.stringify(cin) + '<EOF>');
+               break;
+           }
+           if(upload_arr[i].ctname == 'cnt-humid') {
+               var cin = {ctname: upload_arr[i].ctname, con: readout.humidity.toFixed(2)};
+               console.log('SEND : ' + JSON.stringify(cin) + ' ---->');
+               upload_client.write(JSON.stringify(cin) + '<EOF>');
+               break;
+           }
+       }
+   }
+
+   console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + 'humidity: ' + readout.humidity.toFixed(2) + '%');
+   setTimeout(function () {
+     sensor.read();
+   }, 2000);
+ }
+};
+
+if (sensor.initialize()) {
+ sensor.read();
+} else {
+ console.warn('Failed to initialize sensor');
+}
+
+
+upload_client.on('error', function(err) {
+    tas_state = 'reconnect';
+});
+
+upload_client.on('close', function() {
+    console.log('Connection closed');
+    upload_client.destroy();
+    tas_state = 'reconnect';
+});
